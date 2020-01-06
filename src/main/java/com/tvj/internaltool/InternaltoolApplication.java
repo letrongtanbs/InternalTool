eede785766bbc85e4c16c11f5535f22948fcaf3c
dexdinh91 <ngocdc1991@gmail.com>
2020-01-06 14:51:17 +0900

    init project

---
+++ src/main/java/com/tvj/internaltool/InternaltoolApplication.java
@@ -0,0 +1,13 @@
+package com.tvj.internaltool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternaltoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternaltoolApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/tvj/internaltool/controller/UserController.java
@@ -0,0 +1,8 @@
+package com.tvj.internaltool.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+}
